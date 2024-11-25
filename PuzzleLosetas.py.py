995da859d0fe5ea1579f3dd5897c52ed4502f8ae
Joha Varoli <111708771+Johavaroli@users.noreply.github.com>
2024-11-25 18:18:53 +0100

    Update PuzzleLosetas.py.py

--- PuzzleLosetas.py.py
+++ PuzzleLosetas.py.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-@author: Johanna Varoli
+Spyder Editor
+
+Johanna Vargas.
+
 """
 import time
 import heapq
@@ -8,7 +11,7 @@ import heapq
 # definimos la clase resolver
 class PuzzleSolver:
     """
-    Esta clase representa un solucionador de puzzle.
+    Esta clase representa una solución al puzzle de las losetas.
     
     """
     def __init__(self, estado_inicial, estado_objetivo):
@@ -21,14 +24,14 @@ class PuzzleSolver:
 
     def is_valid_move(self, row, col):
         """
-       método que verifica si la movida está dentro de los límites del tablero.
+       Este método verifica si el movimiento está dentro de los límites del tablero.
         
         """
         return 0 <= row < self.size and 0 <= col < self.size
 
     def generate_moves(self, state):
         """
-        método que genera las posibles movidas a partir del estado actual.
+        Este método genera los posibles movimientos a partir del estado actual.
         
         """
         zero_pos = [(i, row.index(0)) for i, row in enumerate(state) if 0 in row][0]
@@ -39,7 +42,7 @@ class PuzzleSolver:
 
     def apply_move(self, state, move):
         """
-        método que aplica una movida al estado actual para obtener un nuevo estado.
+        Este método aplica un movimiento al estado actual para generar un nuevo estado.
         
         """
         row, col = move
@@ -50,7 +53,7 @@ class PuzzleSolver:
 
     def is_goal_state(self, state):
         """
-        método que verifica si el estado actual es el estado objetivo.
+        Este método verifica si el estado actual es el estado objetivo.
         
         """
         return state == self.estado_objetivo
@@ -68,7 +71,7 @@ class PuzzleSolver:
         while priority_queue:
             cost, current_state, path = heapq.heappop(priority_queue)
 
-            # Si se encuentra el estado objetivo, se imprime el tiempo, la solución y el número total de movimientos.
+        # Si se encuentra el estado objetivo, se imprime el tiempo, la solución y el número total de movimientos.
             if self.is_goal_state(current_state):
                 end_time = time.time()
                 print(f"la solución fue encontrada en: {end_time - start_time:.5f} segundos.")
@@ -83,30 +86,30 @@ class PuzzleSolver:
             # el estado actual se marca como visitado.
             visited_states.add(current_state)
 
-            # acá se generan las posibles movidas y se agregan a la cola de prioridad.
+            # En este segmento se generan los posibles movimientos y se agregan a la cola de prioridad.
             for move in self.generate_moves(current_state):
                 new_state = self.apply_move(current_state, move)
                 new_path = path + [move]
                 heapq.heappush(priority_queue, (cost + 1, new_state, new_path))
 
-                # en esta parte se muestra cada movimiento paso a paso.
+                #  En este segmento se muestra el paso a paso de cada movimiento.
                 print(f"\nMovimiento {len(new_path)}: Mueve la ficha de 0 a {move}")
 
                 print_puzzle(new_state)
 
-        # se imprime un mensaje si no se encuentra solución.
+        #  En este segmento se imprime el mensaje si no se encuentra solución.
         print("No se encontró la solución.")
         return None
 
 def print_puzzle(state):
     """
-    Función para imprimir el tablero de manera más legible.
+    Esta función imprime en el tablero el método más legible.
     
     """
     for row in state:
         print(row)
 
-# Ejemplo de un puzle inicial desordenado que se puede solucionar:
+# Ejemplo de un puzzle inicial en desorden que muestra la solución:
 initial_state = (
     (5, 7, 2, 0),
     (1,13, 3, 10),
